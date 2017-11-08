b1085202d06a52093bd1f19e912ea6a3433dd1c6
xjq熊剑桥 <jqxiong@Ctrip.com>
2017-11-08 19:46:33 +0800

    update stream

--- stream/index.js
+++ stream/index.js
@@ -3,10 +3,21 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
+// 基于流和管道的渲染
 const server = http.createServer((req, res) => {
     let stream = fs.createReadStream(path.join(__dirname, 'index.html'), 'utf8');
     stream.pipe(res);
 })
+// 文件可能会被阻塞的渲染
+// const server = http.createServer((req, res) => {
+//     fs.readFile(path.join(__dirname, 'index.html'), 'utf8', (err, content) => {
+//         if (err) {
+//             res.end(err);
+//         } else {
+//             res.end(content)
+//         }
+//     })
+// })
 
 server.listen(8088);
-console.log('server is running on port 8080');
+console.log('server is running on port 8088');

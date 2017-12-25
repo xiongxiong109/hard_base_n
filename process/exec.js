a64dc48ed6b10d4af7568938d208a675c8088425
xiongxiong109 <444122662@qq.com>
2017-12-25 23:28:11 +0800

    spawn

---
+++ process/exec.js
@@ -0,0 +1,15 @@
+// 执行子进程
+const cp = require('child_process');
+// 提供一个回调函数
+// cp.exec('cat ./child.js', (err, stdout, strerr) => {
+// 	if (err) {
+// 		return new Error(err)
+// 	}
+// 	console.log(stdout);
+// });
+
+// 基于流的spawn, 第一个参数传递指令, 后面的数组传递指令执行的参数
+let stream = cp.spawn('cat', ['./child.js']);
+// let stream = cp.spawn('fis3', ['-v']);
+
+stream.stdout.on('data', data => console.log(data.toString()));

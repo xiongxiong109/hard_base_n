099ab3d2c9bd0322e06f2487fff4ecb602678c92
xjq熊剑桥 <jqxiong@Ctrip.com>
2018-01-19 10:31:17 +0800

    express nconf

---
+++ webworks/server.js
@@ -0,0 +1,44 @@
+// 使用配置信息来更好地维护项目
+// console.log(process.env); // env设置环境变量
+// console.log(process.argv); // argv设置命令行参数
+/**
+ * env参数要放在node指令之前, 作为node启动的前置环境变量
+ * 放在node 指令之后的参数，都会依次push到process.argv中去
+ */
+// example
+// NODE_ENV=production node ./server -s aa
+// 打印出来process.env.NODE_ENV = production
+// argv[2] = s; argv[3] = aa;
+
+// 使用nconf来维护配置信息
+const http = require('http');
+const path = require('path');
+const express = require('express');
+const nconf = require('nconf');
+// nconf configs
+nconf
+  .env()
+  .argv()
+  .file('appSettings', path.join(__dirname, '..', 'package.json'));
+
+let ENV = nconf.get('NODE_ENV');
+let PORT = nconf.get('PORT');
+let appSettings = nconf.get('appSettings');
+console.log(appSettings);
+const port = PORT || 8080;
+const app = express();
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.send(Object.assign({}, {
+    ENV, port
+  }, appSettings));
+});
+
+app.use('/', router);
+
+const server = http.createServer(app);
+server.listen(port, (err) => {
+  if (err) {return new Error(err.stack)}
+  console.log(`server is running on port ${port}`);
+});

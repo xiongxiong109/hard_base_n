8027081a4bdddeac1b176134ec405a663ebba6df
xjq熊剑桥 <jqxiong@Ctrip.com>
2018-02-06 09:59:01 +0800

    on error for stack

--- error_stack/index.js
+++ error_stack/index.js
@@ -6,4 +6,11 @@ const { EventEmitter } = require('events');
 const ee = new EventEmitter();
 
 // ee.emit('error', 'a test for emit error'); // error 事件必须由new Error传递, 否则就是 uncaught error
-ee.emit('error', new Error('a test for emit error'));
+
+// 2. 监听error事件, 这样可以跟踪到堆栈信息
+ee.on('error', err => {
+  console.log(err.message);
+  console.log(err.stack);
+});
+
+// ee.emit('error', new Error('a test for emit error'));

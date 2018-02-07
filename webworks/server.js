00e787f31477cafdb567cd2499e533e4b27578a6
xjq熊剑桥 <jqxiong@Ctrip.com>
2018-02-07 10:24:32 +0800

    node debugger

--- webworks/server.js
+++ webworks/server.js
@@ -1,3 +1,7 @@
+// 在node中调试应用 (node debug 启动应用, repl可以停留在当前上下文中)
+// node --prof 启动应用, 会记录v8.log日志, 记录的日志可以通过tick模块查看
+// 在日志中的Javascript部分能让我们分析出程序中调用频繁、计算频繁的地方
+// 通过node --inspect启动, 可以连接chrome devtools
 const http = require('http')
 const app = require('./app')
 const server = http.createServer(app)

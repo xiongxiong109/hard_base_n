008d4f33a1022d0d2b878f610af98e42a611fce1
xiongxiong109 <444122662@qq.com>
2018-03-10 23:12:18 +0800

    数据结构 列表

--- benchmark/ab_test.js
+++ benchmark/ab_test.js
@@ -4,23 +4,27 @@ const Benchmark = require('benchmark');
 let suite = new Benchmark.Suite;
 
 // 字符串拼接函数测试
-const { joinByArr, joinByStr } = require('./join');
+const {
+    joinByArr, joinByStr 
+} = require('./join');
 
 let str1 = 'hello';
 let str2 = 'world';
 let str3 = 'pp';
 
 suite
-  .add('joinByArr', function() {
-    joinByArr(str1, str2, str3);
-  })
-  .add('joinByStr', function() {
-    joinByStr(str1, str2, str3);
-  })
-  .on('cycle', function(event) {
-    console.log(String(event.target));
-  })
-  .on('complete', function() {
-    console.log(`Fastest: ${this.filter('fastest').map('name')}`)
-  })
-  .run({async: true})
+    .add('joinByArr', function() {
+        joinByArr(str1, str2, str3);
+    })
+    .add('joinByStr', function() {
+        joinByStr(str1, str2, str3);
+    })
+    .on('cycle', function(event) {
+        console.log(String(event.target));
+    })
+    .on('complete', function() {
+        console.log(`Fastest: ${this.filter('fastest').map('name')}`)
+    })
+    .run({
+        async: true
+    })
---
+++ datastructure/List.js
@@ -0,0 +1,28 @@
+// 数据结构 列表
+
+class List {
+    constructor(props) {
+        this._dataStore = []; // 列表数据
+        this._curIdx = 0; // 当前索引
+    }
+    first() { // 访问列表的头部元素
+
+    }
+    last() { // 访问列表的最后一个元素
+
+    }
+    insertBefore(index = 0) { // 在列表的某一位置插入元素
+        
+    }
+    prev() { // 列表的上一个元素
+
+    }
+    next() { // 列表的下一个元素
+
+    }
+    toString() { // 返回列表本身
+
+    }
+}
+
+module.exports = List
